df640f4097662225f1889256950c47e81fed08af
MadhusudhanReddyBoddu <mb0012@mix.wvu.edu>
2017-08-06 15:39:32 -0400

    added registration documents

---
+++ src/scripts/registration.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from "jquery"; //var $ = require("jquery");
+
+require("!style-loader!css-loader!./../css/registration.css");
+// // require('/../css/registration.css');
+
+// require("file-loader?name=index.html!./../views/index.html");
+
